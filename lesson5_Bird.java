02d4be1d5cd773d6c544e1b19726e4da001ce223
Леонид Л <59210432+Lezhenleo@users.noreply.github.com>
2020-04-20 17:34:12 +0300

    //

--- lesson5_Bird.java
+++ lesson5_Bird.java
@@ -1,5 +1,5 @@
 package ru.homework.lesson5;
-
+//Птица
 public class Bird extends Animals{
     public Bird(String name, double max_length, double max_length_swim, double max_height) {
         super(name, max_length, max_length_swim, max_height);
