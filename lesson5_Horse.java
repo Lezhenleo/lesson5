9b2cd02dc5bbe812029849384c32c0b241442983
Леонид Л <59210432+Lezhenleo@users.noreply.github.com>
2020-04-20 17:24:42 +0300

    //

--- lesson5_Horse.java
+++ lesson5_Horse.java
@@ -1,5 +1,5 @@
 package ru.homework.lesson5;
-
+//Лошадь 
 public class Horse extends Animals{
     public Horse(String name, double max_length, double max_length_swim, double max_height) {
         super(name, max_length, max_length_swim, max_height);
