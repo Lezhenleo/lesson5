a9a78d3ccc7880b329addd7daf43b1f49c90e0c6
Леонид Л <59210432+Lezhenleo@users.noreply.github.com>
2020-04-20 17:31:52 +0300

    //

--- lesson5_Dog.java
+++ lesson5_Dog.java
@@ -1,5 +1,5 @@
 package ru.homework.lesson5;
-
+//Собака
 public class Dog extends Animals{
     public Dog(String name, double max_length, double max_length_swim, double max_height){
         super(name, max_length, max_length_swim, max_height);
